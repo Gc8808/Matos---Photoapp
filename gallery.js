70bc316fd888c9f47054a645cffd9ff29e974578
Gc8808 <526gmatos@frhsd.com>
2025-11-04 13:57:31 +0000

    created files

--- gallery.js
+++ gallery.js
@@ -22,9 +22,14 @@ $(document).ready(() => {
 
 // Function to fetch JSON data and store it in mImages
 function fetchJSON () {
-  // Use $.ajax here to request the JSON data from mUrl
-  // On success, parse the JSON and push each image object into mImages array
-  // After JSON is loaded, call swapPhoto() to display the first image
+  fetch(mUrl)
+    .then(response => response.json())
+    .then(data => {
+    console.log(data)
+
+    .catch(error => {
+      console.error('Error fetching JSON data:', error)
+    })})
 }
 
 // Function to swap and display the next photo in the slideshow
